85617a6dddb76bbc9e238b05dbd5ccb467459aa8
Kanamahk <tristan.hermant@u-psud.fr>
2017-11-29 09:59:42 +0100

    ajout ACP

--- donnees.py
+++ donnees.py
@@ -3,6 +3,16 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
+
+def getJustImages(data):
+	if(data == "test"):
+		return np.load("data/tst_img.npy")
+	if(data == "dev"):
+		return np.load("data/dev_img.npy")
+	if(data == "training"):
+		return np.load("data/trn_img.npy")
+
+
 def lectureDonnneesTuple(data):
 	if(data == "test"):
 		X0 = np.load("data/tst_img.npy")
@@ -102,8 +112,9 @@ def convertLbl(lbl):
 		return 'Ankle boot'
 
 def createAverageImage(groupeImage):
-	res = np.zeros(784)
-	for i in range(0, 784):
+	tailleImage = len(groupeImage[0])
+	res = np.zeros(tailleImage)
+	for i in range(0, tailleImage):
 		for j in range(0, len(groupeImage)):		
 			res[i] += groupeImage[j][i]
 		res[i] /= len(groupeImage)
@@ -132,50 +143,88 @@ def afficheAverageImages(Dict):
 
 def calcDifference(image, averageImage):
 	res = 0
-	for i in range(0, 784):
+	for i in range(0, len(image)):
 		temp = averageImage[i] - image[i]
 		res += -temp if temp < 0 else temp
 	return res
 
 def calcAllDifference(image, averageImages):
 	total = 0
-	tailleDuPlusPetit = 255*784 #difference max
+	tailleDuPlusPetit = 255*len(image) #difference max
 	plusPetit= 0;
-	Dict = {}
+	Liste = []
 	for i in range(0, len(averageImages)):
-		Dict[i] = calcDifference(image, averageImages[i])
-		if Dict[i] < tailleDuPlusPetit :
-			tailleDuPlusPetit = Dict[i]
+		Liste.append(calcDifference(image, averageImages[i]))
+		if Liste[i] < tailleDuPlusPetit :
+			tailleDuPlusPetit = Liste[i]
 			plusPetit = i
-		total += Dict[i]
-	return (plusPetit, Dict, total)
+		total += Liste[i]
+	return (plusPetit, Liste, total)
 
-def makeAGuess(data, averageImages):
+def makeAGuess(data, averageImages, string, Dict):
 	difs = calcAllDifference(data[0], averageImages)
-	#proba = 100 * (1- difs[1][difs[0]]/difs[2])
-	#print('Je pense que ce vetement est un ' + convertLbl(difs[0]) + ' avec une probabilite de ' + str(proba) + '%.')
-	print('Je pense que ce vetement est un ' + convertLbl(difs[0]) + '.')
+	#print('Je pense que ce vetement est un ' + convertLbl(difs[0]) + '.')
+	string = string + 'Je pense que ce vetement est un ' + convertLbl(difs[0]) + '.\n'
+
 	if difs[0] == data[1]:
-		print('Ce vetement est effectivement un ' + convertLbl(difs[0]) + '.')
+		#print('Ce vetement est effectivement un ' + convertLbl(difs[0]) + '.')
+		string = string + 'Ce vetement est effectivement un ' + convertLbl(difs[0]) + '.\n'
+		
+		Dict[difs[0]]['nbFound'] += 1
 		res = 1
 	else:
-		print('Ce vetement etait en fait un ' + convertLbl(data[1]) + '.')
+		#print('Ce vetement etait en fait un ' + convertLbl(data[1]) + '.')
+		string = string + 'Ce vetement etait en fait un ' + convertLbl(data[1]) + '.\n'
 		res = 0
-	print()
-	return res
+
+	Dict[data[1]]['nbTotal'] += 1
+	Dict[data[1]][difs[0]] += 1
+
+	#print()
+	return (res, string)
 
 def makeAllGuess(datas, averageImages):
 	nbReconnu = 0
-	for i in range(0, len(datas)):
-		nbReconnu += makeAGuess(datas[i], averageImages)
-	print('\nJ\'ai reconnu ' + str(nbReconnu) + ' vetements sur ' + str(len(datas)) + ' (' + str(100 * nbReconnu/len(datas)) + '%).')
+	CofusionMatrice =  {0 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						1 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						2 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						3 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						4 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						5 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						6 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						7 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						8 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0},
+						9 : {'nbTotal' : 0, 'nbFound' : 0, 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0}}
+	
+	res = (0, "")
+
+	for i in range(0, len(datas[0])):
+		res = makeAGuess((datas[0][i], datas[1][i]), averageImages, res[1], CofusionMatrice)
+		nbReconnu += res[0]
+
+	string = res[1]
+
+	for i in range(0, len(CofusionMatrice)):
+		string = string + '\nJ\'ai reconnu ' + str(CofusionMatrice[i]['nbFound']) + ' ' + convertLbl(i) +  ' sur ' + str(CofusionMatrice[i]['nbTotal']) + ' (' + str(100 * CofusionMatrice[i]['nbFound']/CofusionMatrice[i]['nbTotal']) + '%).\n'
+		for j in range(0, len(CofusionMatrice)):
+			if(i != j):
+				string = string + 'J\'ai confondu ' + str(CofusionMatrice[i][j]) + ' ' + convertLbl(j) + ' pour des ' + convertLbl(i) + '.\n'
 		
+
+	#print('\nJ\'ai reconnu ' + str(nbReconnu) + ' vetements sur ' + str(len(datas)) + ' (' + str(100 * nbReconnu/len(datas)) + '%).')
+	string = string + '\nJ\'ai reconnu ' + str(nbReconnu) + ' vetements sur ' + str(len(datas[0])) + ' (' + str(100 * nbReconnu/len(datas[0])) + '%).'
+	write_in_file(string, "res.txt")
+	
+
 		
 	
 	
 
 
-
+def write_in_file(string, fileName):
+	file_pointer = open(fileName, "w")
+	file_pointer.write(string)
+	file_pointer.close()
 
 
 
--- main.py
+++ main.py
@@ -3,23 +3,36 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 from donnees import * 
+from sklearn.decomposition import PCA
 
 
 #if __name__=="__main__":
 
 
-donnees = lectureDonnneesTuple("dev")
+tabImages = getJustImages("training")
+pca = PCA(600, True)
+pca.fit(tabImages)
+
+
+donnees = lectureDonnneesTuple("training")
+imagesTransforme = pca.transform(donnees[0])
+donnees = (imagesTransforme, donnees[1])
+
 dictDonnees = tupleDonnneesToDictList(donnees)
 averageImagesDict = createAverageImages(dictDonnees)
 
-donnesAReconnaitre = lectureDonnneesList("test")
+
+donnesAReconnaitre = lectureDonnneesTuple("test")
+imagesTransforme2 = pca.transform(donnesAReconnaitre[0])
+donnesAReconnaitre = (imagesTransforme2, donnesAReconnaitre[1])
 
 makeAllGuess(donnesAReconnaitre, averageImagesDict)
 
-'''
-#afiche les averageImage de chaque classe
-afficheAverageImages(averageImagesDict)
-'''
 
+#afiche les averageImage de chaque classe
+'''
+afficheAverageImages(averageImagesDict)
+
+'''
 
 
